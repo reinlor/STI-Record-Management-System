c3ab1606bac3e81995af042a8ecd65f18047e537
Rehneil Lor <raykiller456@gmail.com>
2025-07-08 20:01:42 +0800

    Joi Framework

--- server/controller/studentController.js
+++ server/controller/studentController.js
@@ -1,11 +1,99 @@
 const { getStudentCollection } = require("../models/studentModel.js");
+const Joi = require('joi');
+
+// Student Schema
+const studentSchema = Joi.object({
+  sid:            Joi.string().required(),
+
+  studentProfile: {
+    name:         Joi.string().required(),
+    section:      Joi.string().required(),
+    age:          Joi.number().required(),
+    nationality:  Joi.string().required(),
+    gender:       Joi.string().required(),
+    status:       Joi.string().required(),
+    birthPlace:   Joi.string().required(),
+    birthday:     Joi.date().required(),
+    religion:     Joi.string().required()
+  },
+
+  contactInfo: {
+    email:        Joi.string().email().required(),
+    contactNo:    Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')),
+    address: {
+      permanentAddress:  Joi.string().required(),
+      currentAddress:    Joi.string().required(),
+      provincialAddress: Joi.string()
+    }
+  },
+
+  familyBackground:{
+    fatherInfo: {
+      name:        Joi.string(),
+      age:         Joi.number(),
+      nationality: Joi.string(),
+      religion:    Joi.string(),
+      educationalAttainment: Joi.string(),
+      occupation:  Joi.string(),
+      company:     Joi.string()
+    },
+    motherInfo: {
+      name:        Joi.string(),
+      age:         Joi.number(),
+      nationality: Joi.string(),
+      religion:    Joi.string(),
+      educationalAttainment: Joi.string(),
+      occupation:  Joi.string(),
+      company:     Joi.string()
+    },
+    monthlyFamilyIncome: Joi.number(),
+    statusOfParent:       Joi.string(),
+    siblingOrder:         Joi.array(),
+    emergency: {
+      name:        Joi.string(),
+      contactNo:   Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$'))
+    }
+  },
+
+  educationalBackground: {
+    dateEnrolled:  Joi.date(),
+    seniorHighSchool: {
+      schoolName:  Joi.string(),
+      dateEnrolled:Joi.date(),
+      withHonors:  Joi.boolean()
+    },
+    juniorHighSchool: {
+      schoolName:  Joi.string(),
+      dateEnrolled:Joi.date(),
+      withHonors:  Joi.boolean()
+    },
+    elementary: {
+      schoolName:  Joi.string(),
+      dateEnrolled:Joi.date(),
+      withHonors:  Joi.boolean()
+    },
+  },
+
+  hobbies:         Joi.array(),
+
+  health: {
+    currentConcerns: Joi.array(),
+    otherConcerns:  Joi.string()
+  },
+
+  lifeCircumstances: Joi.array()
+})
 
 // Controller Function for adding student data 
 const addStudent = async (req, res) => {
   try {
-    const newStudent = req.body;
-    const sid = newStudent.sid;
+    studentSchema.validate(req.body)
 
+    const { error, value: newStudent } = studentSchema.validate(req.body);
+    if (error) {
+        return res.status(400).json({ error: error.details[0].message });
+    }
+    const sid = newStudent.sid;
     await getStudentCollection().doc(sid).set(newStudent);
     
     res.status(201).json({ 
