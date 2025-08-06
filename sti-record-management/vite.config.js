1ee7ab344a4cb16342ba4c29ad8a346edc8d1c75
Rehneil Lor <raykiller456@gmail.com>
2025-08-06 12:37:15 +0800

    di ko alam kung anong bago

--- sti-record-management/vite.config.js
+++ sti-record-management/vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    tailwindcss(),
+    react()],
+  server: {
+    proxy: {
+      "/student": "http://localhost:5000",
+      "/slip": "http://localhost:5000",
+      "/referral": "http://localhost:5000",
+      "/violation": "http://localhost:5000",
+    },
+  },
 })
