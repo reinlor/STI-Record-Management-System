a2b64ff18e5e7ef93303d5d7c381219ba468418e
Rehneil Lor <raykiller456@gmail.com>
2025-10-29 14:41:32 +0800

    Vite config modification

--- sti-record-management/vite.config.js
+++ sti-record-management/vite.config.js
@@ -4,12 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./",
+  base: "/",
   build: {
     sourcemap: false,
   },
   server: {
     allowedHosts: ['sti-gorms.duckdns.org'],
+    host: true,
     proxy: {
        
     },
